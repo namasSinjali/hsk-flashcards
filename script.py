b93943c2f82fadebdb56cddb05a192212a5b7460
namasSinjali <namas22thapa@gmail.com>
2024-01-20 19:52:59 +0800

    first working

---
+++ script.py
@@ -0,0 +1,16 @@
+chars = {}
+with open('hsk4.txt', encoding='utf-8') as hsk,  open("out.txt", 'w', encoding='utf-8') as out:
+    lines = hsk.readlines()
+    for line in lines:
+        if line.startswith('//'):
+            chars.setdefault(line.strip(), 0)
+            chars[line.strip()]+=1
+        else:
+            for char in line:
+                if char != '\n':
+                    chars.setdefault(char, 0)
+                    chars[char]+=1
+
+    for el in chars:
+        # out.write(f'{el}\t{chars[el]}\n')
+        out.write(f'{el}\n')
